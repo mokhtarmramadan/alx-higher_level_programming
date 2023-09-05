96fd8de0f0f89e4ff14df5a2a7b6154a99458773
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-05 20:59:19 +0300

    another approch

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-flage = False
-for i in range(122, 96, -1):
-    if not flage:
-        flage = True
-        print(chr(i), end='')
-    else:
-        flage = False
-        print(chr(i - 32), end='')
+def main():
+
+    flage = False
+    for i in range(122, 96, -1):
+        if not flage:
+            flage = True
+            print_rettel(i)
+        else:
+            flage = False
+            print_rettel(i - 32)
+
+
+def print_rettel(num):
+    print(chr(num), end='')
+
+
+if __name__ == "__main__":
+    main()
