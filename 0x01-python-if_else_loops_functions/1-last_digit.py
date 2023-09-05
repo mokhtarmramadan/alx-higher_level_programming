7aac0f282bfcecc1c7246301eb5a0a3e74ece7b7
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-05 13:15:03 +0300

    Added print alphabet.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,15 @@ if last_digit == 0:
     print(f"Last digit of {number:d} is 0 and is 0")
 elif last_digit > 5:
     if number > 0:
-        print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
+        print(f"Last digit of {number:d} is "
+            f"{last_digit:d} and is greater than 5")
     else:
-        print(f"Last digit of {number:d} is -{last_digit:d} and is greater than 5")
+        print(f"Last digit of {number:d} is "
+        f"-{last_digit:d} and is greater than 5")
 else:
     if number > 0:
-        print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
+        print(f"Last digit of {number:d} is "
+        f"{last_digit:d} and is less than 6 and not 0")
     else:
-        print(f"Last digit of {number:d} is -{last_digit:d} and is less than 6 and not 0")
+        print(f"Last digit of {number:d} is "
+        f"-{last_digit:d} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+alphabets = [chr(i) for i in range(ord('a'), ord('z') + 1)]
+for a in alphabets:
+    print(a, end='')
