192aa6f149a99a7370dbf5b793daa21afb32465e
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-05 16:51:17 +0300

    Added uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    str = str + '\n'
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            print("{}".format(chr(ord(i) - 32)), end='')
+        else:
+            print(i, end='')
