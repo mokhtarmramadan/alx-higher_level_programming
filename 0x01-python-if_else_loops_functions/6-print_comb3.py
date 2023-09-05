a8a79f61100212f78959c302b2b2cc29600e5d62
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-05 15:37:38 +0300

    Added print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(i + 1, 10):
         if i == 8:
-            print("89")
+            print("{}{}".format(i, j))
             break
-        print(f"{i}{j}, ", end='')
+        print("{}{}, ".format(i, j), end='')
