0cc19b82328ad0044ede051c62ca26a392f92252
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-07 19:53:21 +0300

    Added 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+
+    if (len(argv) - 1) <= 0:
+        print("0 arguments.")
+        exit()
+
+    print("{} arguments:".format((len(argv) - 1)))
+
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
