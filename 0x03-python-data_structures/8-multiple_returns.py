4777888fe8ad6c76ddd0ecb1ae93afeeef7f8f98
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-10 17:39:19 +0300

    Hot fix

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
+    if length == 0:
+        return (0, None)
     first_char = sentence[0]
     return (length, first_char)
