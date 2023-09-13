dc6a85e3abfb64edf7f0ccd0eecb8628358dc345
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-13 22:14:36 +0300

    Added multiplyby2.py

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    key_list = list(a_dictionary.keys())
+    
+    if key in key_list:
+        a_dictionary[key] = value
+    else:
+        a_dictionary.update( {key, value} )
