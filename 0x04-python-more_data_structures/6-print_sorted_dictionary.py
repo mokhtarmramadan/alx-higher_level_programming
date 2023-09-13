47fafaff57924cafe9c4da72628e1539b46a74d5
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-13 19:17:36 +0300

    Added sorted dict

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    myKeys = list(a_dictionary.keys())
+    myKeys.sort()
+    sorted_dict = {i: a_dictionary[i] for i in myKeys}
+    for item in sorted_dict:
+        print("{} : {}".format(item, sorted_dict[item]))
