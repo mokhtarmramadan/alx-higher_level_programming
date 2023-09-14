9b97c505b2eb78b0ac6b81c3321fb4fbc59adec2
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-14 19:16:15 +0300

    Added roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romans = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            }
+    sum = 0
+    prev = 0
+
+    for num in roman_string:
+        if romans[num] > prev and prev != 0:
+            sum += (-2 * prev) + romans[num]
+            prev = romans[num]
+        elif prev == 0:
+            sum = romans[num]
+            prev = romans[num]
+        else:
+            sum += romans[num]
+            prev = romans[num]
+
+    return abs(sum)
