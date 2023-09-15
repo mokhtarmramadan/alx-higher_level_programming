6b9ce1ab673fdbd703dc7c406b6eea429467cd53
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-15 17:33:52 +0300

    Hot fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    key_list = list(a_dictionary.keys())
-    
-    if key in key_list:
-        a_dictionary[key] = value
-    else:
-        a_dictionary.update( {key, value} )
+    a_dictionary[key] = value
+    return (a_dictionary)
