c9ea70a2aa54715f3d87251a2880dbafd92ca5f7
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-15 17:51:56 +0300

    Hot fix

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    result = 0
+    if len(my_list) == 0:
+        return 0
+    result = 0 
     numerator = 0
     denominator = 0
 
     for tup in my_list:
         numerator += tup[0] * tup[1]
         denominator += tup[1]
-    result = numerator / denominator
+    result = float(numerator) / float(denominator)
     return result
