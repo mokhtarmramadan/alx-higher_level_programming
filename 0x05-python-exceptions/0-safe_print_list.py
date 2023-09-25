05c733e2c01fa01e1ac3255abed031b9ef8507de
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-25 15:02:46 +0300

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter = 1
+    counter = 0
     for i in range(x):
         try:
             print(my_list[i], end='')
+            counter += 1
         except IndexError:
-            counter -= 1
-            pass
-    counter += i
+            break
     print()
     return counter
