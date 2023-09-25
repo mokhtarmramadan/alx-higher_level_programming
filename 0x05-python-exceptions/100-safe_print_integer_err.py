557c1a2de937e4405934d5594f24da7da14a0d22
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-25 13:18:05 +0300

    Adde safe print

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except ValueError:
+
--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -4,4 +4,4 @@ raise_exception = __import__('5-raise_exception').raise_exception
 try:
     raise_exception()
 except TypeError as te:
-    print("Exception raised")
+    print(te)
