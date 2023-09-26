8d8f0b7527f7c68b161a62fb53ded3890906a1fb
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-26 14:39:46 +0300

    added 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a class Square that defines a square by: (based on 2-square.py) """
+
+
+class Square:
+
+    ''' Instantiation with optional size '''
+    def __init__(self, size=0):
+        self.__size = size
+
+    ''' Public instance method: area that returns the current square area '''
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
