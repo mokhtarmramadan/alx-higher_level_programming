c3e1cc0da0c377bfa272a4a4ac328d0ccf627a08
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-09-26 12:49:30 +0300

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""  class Square that defines a square """
+
+
+class Square:
+    """ Private instance attribute: size """
+
+    def __init__(self, size):
+        self.__size = sizei
