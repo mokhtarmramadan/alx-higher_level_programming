1b13606e397cf6c493633821899e2b2dd35d8108
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-10-10 17:29:36 +0300

    Fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-     """Write a string to a UTF8 text file."""
-    with open(filename, 'w+', encoding="utf-8") as file:
+    """Write a string to a UTF8 text file."""
+    with open(filename, 'w', encoding="utf-8") as file:
         return file.write(text)
