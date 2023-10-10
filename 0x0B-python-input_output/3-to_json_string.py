f42417d362d70962029914e1c7aea17fc1c14d4b
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-10-10 17:37:34 +0300

    Added to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" This module supplies one fucntion """
+
+
+def to_json_string(my_obj):
+    """ returns a JSON representation of an object """
+    return json.dumps(my_obj)
