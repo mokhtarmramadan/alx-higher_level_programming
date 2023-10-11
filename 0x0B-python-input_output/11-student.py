409bf4aaed895bd40d1069d2a2da877dc8a3ab50
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-10-11 17:44:13 +0300

    Added student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module supplies one class"""
+
+
+class Student:
+    """ Student class """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the student class """
+        if (type(attrs) is list and
+                all(type(ele) is str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the student class"""
+        for k, v in json.items():
+            setattr(self, k, v)
