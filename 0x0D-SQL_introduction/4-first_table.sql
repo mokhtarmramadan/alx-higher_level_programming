cac3e55a512c45b99f43e6bb4f310c1fa2d64aa5
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2023-11-14 22:13:30 +0200

    Added 5-full_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -6,7 +6,6 @@
 -- The database name will be passed as an argument of the mysql command
 -- If the table first_table already exists, your script should not fail
 -- You are not allowed to use the SELECT or SHOW statements
-CREATE TABLE IF NOT EXISTS first_table (
-	id INT,
-	name VARCHAR(256)
-);
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'first_table';
