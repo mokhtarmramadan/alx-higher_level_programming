aefee9fca9f8b1dbf111c1e7b64964c3d5cb4564
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-01-08 18:12:13 +0200

    Added  8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const argv = process.argv.slice(2);
+const number = parseInt(argv);
+
+if (!argv[0] || isNaN(number)) {
+  console.log('Missing size');
+} else if (number > 0) {
+  let asterisk = '';
+  for (let i = 0; i < number; i++) {
+    for (let j = 0; j < number; j++) {
+      asterisk += '*';
+    }
+    console.log(asterisk);
+    asterisk = '';
+  }
+}
