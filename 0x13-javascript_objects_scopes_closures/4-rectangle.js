bf7c7201fa47ad9c571d7355f6f37fb17a901d61
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-01-09 12:13:43 +0200

    Added 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -17,7 +17,17 @@ class Rectangle {
       console.log(x);
     }
   }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = 2 * this.width;
+    this.height = 2 * this.height;
+  }
 }
 
 module.exports = Rectangle;
-~                           
