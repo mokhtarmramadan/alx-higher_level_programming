a2c940c6929640b15a32bd9f0554a8a1005cec0e
mokhtarmramadan <mokhtarramdanformal@gmail.com>
2024-01-27 19:24:57 +0200

    Added 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body of the response
 
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+curl -s "$1" | wc -c
